643e6e7cb349fa94cbc3fc88e1d55c7b6a262d11
textarcana <noah@onemorebug.com>
2009-05-31 16:13:53 -0400

    very simple Hpricot interface for retrieving strings from an SGML file by tag name

--- hpricot_helper.rb
+++ hpricot_helper.rb
@@ -5,6 +5,10 @@
 # = Description
 # Simple examples of using Hpricot to parse XML.
 
+require 'rubygems'
+
+require 'hpricot'
+
 module HpricotHelper
 
   def read_file target
@@ -17,19 +21,49 @@ module HpricotHelper
     rescue
 
       # ...or die with message
-      puts "usage:    ruby simple.rb <file>"
+      puts "first argument should have been <file>"
       exit 1
 
     end
 
   end
 
-  def dump_file_dom filehandle
-    
-    fh = filehandle
+  def dom_from_file target
 
+    fh = read_file target
+    
     dom = Hpricot(fh.read)
 
   end
 
+  def get_by_tagname dom, tagName
+
+    dom.search(tagName).to_a
+
+  end
+
+  # == retrieve just the text content from all the tags of a certain type
+  def get_articles_by_tagname dom, tagName
+
+    accumulator = []
+
+    articles = get_by_tagname dom, tagName
+
+    articles.each do | raw |
+    
+      accumulator.push raw.inner_text
+  
+    end
+    
+    accumulator
+
+  end
+
 end
+
+# For example:
+#
+#     require 'hpricot_helper'
+#     include HpricotHelper
+#     d = dom_from_file 'sample_data/simpsons.xml'
+#     a = get_articles_by_tagname d, 'familymember'
